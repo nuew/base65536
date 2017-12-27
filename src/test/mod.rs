15a512fa46592bad0fdd195ad067da2582175a91
Nuew <code@nuew.net>
2017-12-27 03:24:00 -0500

    Add Send + Sync tests for Error and WrapOptions

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -11,10 +11,33 @@ mod wrap;
 mod bench;
 
 #[test]
-// this doesn't really fit anywhere else
 fn correct_b2s_types() {
     #[cfg(feature = "fnv")]
     let _: &::fnv::FnvBuildHasher = BLOCK_START_TO_INDEX.hasher();
     #[cfg(not(feature = "fnv"))]
     let _: &::std::collections::hash_map::RandomState = BLOCK_START_TO_INDEX.hasher();
+}
+
+#[test]
+fn error_send() {
+    fn assert_send<T: Send>() {}
+    assert_send::<Error>();
+}
+
+#[test]
+fn error_sync() {
+    fn assert_sync<T: Sync>() {}
+    assert_sync::<Error>();
+}
+
+#[test]
+fn wrapoptions_send() {
+    fn assert_send<T: Send>() {}
+    assert_send::<WrapOptions>();
+}
+
+#[test]
+fn wrapoptions_sync() {
+    fn assert_sync<T: Sync>() {}
+    assert_sync::<WrapOptions>();
 }
